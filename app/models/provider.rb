7ae12f08f5a534d04732b8a5dd259ea45932c450
Brooklyn Zelenka <brooklyn@mobioinsider.com>
2014-05-10 11:51:06 -0700

    Adjusted naming: `capacities` are now `abilities`

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,4 +1,6 @@
 # The organizations, doctor, nurses, or other service provider
 class Provider < ActiveRecord::Base
   attr_accessible :name, :type
+
+  has_many :contacts, :locations, :abilities
 end
