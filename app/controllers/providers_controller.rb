7bdfe3fe32cfeb2128c34356cb831e1235e4baf7
Eric Brooke <ericbrooke@mac.com>
2014-05-10 15:18:01 -0700

    basic providers controller

---
+++ app/controllers/providers_controller.rb
@@ -0,0 +1,71 @@
+class ProvidersController < ApplicationController
+
+before_action :set_provider, only: [:show,:edit, :update,:destroy]
+
+  def index
+    @providers = Providers.all
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render xml: @providers}
+      format.json { render json: @providers}
+    end
+  end
+
+  def show
+    respond_to do |format|
+      format.html
+      format.xml  { render xml: @providers}
+      format.json { render json: @providers}
+    end
+  end
+
+  def new
+      @provider = Provider.new(provider_params)
+  end
+
+  def create
+    if @provider.save
+      flash[:notice] = "Provider has been created."
+      redirect_to @provider
+    else
+      flash[:alert] = "Provider has NOT been created."
+      render "new"
+    end
+
+  end
+
+  def edit
+  end
+
+  def update
+    if @provider.update(provider_params)
+      flash[:notice] = "Provider has been updated."
+      redirect_to @provider
+    else
+      flash[:alert] = "Provider has not been updated."
+      render edit
+    end
+  end
+
+  def destroy
+    @provider.destroy
+
+    flash[:notice] = "Provider has been destroyed."
+    redirect_to providers_path”
+  end
+
+private
+
+  def provider_params
+    params.require(:provider).permit(:name, :type)
+  end
+
+  def set_provider
+    @provider = Provider.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "The provider you were looking" +
+                    " for could not be found."
+      redirect_to providers_path
+  end
+
+end
---
+++ spec/features/creating_providers_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature 'Creating Providers' do
+  scenario "can create a provider" do
+    visit '/'
+
+    click_link 'New Provider'
+
+    fill_in 'Name', with: 'ABC Health'
+    fill_in 'Kind', with 'Health Clinic'
+    click_button 'Create Provider'
+
+    expect(page).to have_content('Provider has been created.')
+  end
+end
