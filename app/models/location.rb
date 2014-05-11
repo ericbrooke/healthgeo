fbda1c8163f3eca88905bf1f635bf5a01cf54407
Brooklyn Zelenka <brooklyn@mobioinsider.com>
2014-05-10 17:23:27 -0700

    Better naming

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,17 +4,17 @@ class Location < ActiveRecord::Base
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 
-  def find_by_radius(point1, point2, radius)
+  def find_by_radius(centre, point, radius)
     acc = []
 
     for ea in self.all do
-      acc.push(ea) if in_circle?(point1, point2, radius)
+      acc.push(ea) if in_circle?(centre, point, radius)
     end
 
     return acc
   end
 
-  def in_circle?(point1, point2, radius)
-    Geocoder::Calculations.distance_between(point1, point2) < radius
+  def in_circle?(centre, point, radius)
+    Geocoder::Calculations.distance_between(centre, point) < radius
   end
 end
