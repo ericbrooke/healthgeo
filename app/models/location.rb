8085500885da350e2f61d0411d393c709b032b95
Brooklyn Zelenka <brooklyn@mobioinsider.com>
2014-05-10 17:50:20 -0700

    Fixed find_all_by_radius

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,20 +1,21 @@
 class Location < ActiveRecord::Base
   has_many :worksites
   has_many :providers, through: :worksites
+
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 
-  def find_by_radius(centre, point, radius)
+  def self.find_all_by_radius(centre, radius)
     acc = []
 
-    for ea in self.all do
-      acc.push(ea) if in_circle?(centre, point, radius)
+    for e in Location.all do
+      acc.push(e) if e.in_circle?(centre, radius)
     end
 
     return acc
   end
 
-  def in_circle?(centre, point, radius)
-    Geocoder::Calculations.distance_between(centre, point) < radius
+  def in_circle?(centre, radius)
+    Geocoder::Calculations.distance_between(centre, self) < radius
   end
 end
