6799508094b9eea6bb1ebaad652f79df25a976f8
Eric Brooke <ericbrooke@mac.com>
2014-05-10 18:21:36 -0700

    CRUD for Locations, no associations with providers

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,6 @@
 # Mostly for interacting with the Geocoder gem
 class LocationsController < ApplicationController
+
   def index
     if params[:search]
       @locations = Location.near(params[:search], 50, order: 'distance')
@@ -17,10 +18,18 @@ class LocationsController < ApplicationController
   end
 
   def create
-    @location = Location.(params[:location])
+    @location = Location.new(location_params)
+
     if @location.save
       redirect_to @location, notice: 'Location saved with much success!'
     else
       render action: 'new'
+    end
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:address, :latitude, :longitude)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
    root 'providers#index'
 
    resources :providers
+   resources :locations
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
