1908054d94b35f2ee01fe0d1cc3d83a9830be83f
Eric Brooke <ericbrooke@mac.com>
2014-05-10 21:49:20 -0700

    added full CRUD to locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,8 @@
 # Mostly for interacting with the Geocoder gem
 class LocationsController < ApplicationController
 
+before_action :set_location, only: [:show,:edit, :update,:destroy]
+
   def index
     if params[:search]
       @locations = Location.near(params[:search], 50, order: 'distance')
@@ -10,7 +12,6 @@ class LocationsController < ApplicationController
   end
 
   def show
-    @location = Location.find(params[:id])
   end
 
   def new
@@ -27,9 +28,36 @@ class LocationsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @location.update(location_params)
+      flash[:notice] = "Location has been updated."
+      redirect_to @location
+    else
+      flash[:alert] = "Location has not been updated."
+      render edit
+    end
+  end
+
+  def destroy
+    @location.destroy
+
+    flash[:notice] = "Location has been destroyed."
+    redirect_to locations_path
+  end
+
   private
 
   def location_params
     params.require(:location).permit(:address, :latitude, :longitude)
+  end
+
+  def set_location
+    @location = Location.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "The location you were looking for could not be found."
+      redirect_to locations_path
   end
 end
